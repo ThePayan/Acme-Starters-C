25e0c1e1ac04b2532f5fc8186433be03375f2062
OnlyPayan <146461708+ThePayan@users.noreply.github.com>
2026-02-15 22:33:51 +0100

    Remove inheritance strategy from AuditReport class

--- src/main/java/acme/entities/auditreport/AuditReport.java
+++ src/main/java/acme/entities/auditreport/AuditReport.java
@@ -23,7 +23,6 @@ import lombok.Setter;
 @Getter
 @Setter
 @Table(name = "AuditReport")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class AuditReport extends AbstractEntity {
 
 	// Serialisation identifier -----------------------------------------------
