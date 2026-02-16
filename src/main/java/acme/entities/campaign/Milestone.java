99c7e98f15415de5fce346a5b6a5d8275720e31e
David Lozano <dlozaco975@gmail.com>
2026-02-16 16:05:57 +0100

    AAAAAA

--- src/main/java/acme/entities/campaign/Milestone.java
+++ src/main/java/acme/entities/campaign/Milestone.java
@@ -5,6 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.validation.Mandatory;
@@ -46,6 +47,7 @@ public class Milestone extends AbstractEntity {
 
 	@Mandatory
 	@ManyToOne
+	@Valid
 	private Campaign			campaign;
 
 }
