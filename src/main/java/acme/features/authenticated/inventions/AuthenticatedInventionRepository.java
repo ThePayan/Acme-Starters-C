4016573389b536b14a33c7d85140c954bbd9adb0
Gregorio Morillo Zamora <gregoriomorilloz@gmail.com>
2026-03-02 16:42:19 +0100

    Update AuthenticatedInventionRepository.java

--- src/main/java/acme/features/authenticated/inventions/AuthenticatedInventionRepository.java
+++ src/main/java/acme/features/authenticated/inventions/AuthenticatedInventionRepository.java
@@ -11,10 +11,10 @@ import acme.entities.inventions.Invention;
 public interface AuthenticatedInventionRepository extends AbstractRepository {
 
 	@Query("select sum(p.cost.amount) from Part p where p.invention.id = :id and p.cost.currency = 'EUR'")
-	public Double getSumOfCosts(int id);
+	Double getSumOfCosts(int id);
 
 	@Query("SELECT COUNT(s) FROM Invention s WHERE s.invention.id = :id")
-	public Integer getNumberOfInventions(int id);
+	Integer getNumberOfInventions(int id);
 
-	public Invention findInventionByTicker(String ticker);
+	Invention findInventionByTicker(String ticker);
 }
